3fbae887fa4942e24b555977694ebc854c0c0c90
NikolaiJamez <nikolaijamez@gmail.com>
2024-07-22 22:25:52 +1200

    Created post creation script

---
+++ newpost.py
@@ -0,0 +1,71 @@
+from datetime import datetime
+from sys import argv
+from os.path import join
+
+
+POST_DIR = r"./_posts/"
+
+
+def safe_str(in_str: str) -> str:
+    out_str = in_str.lower()
+    out_str = out_str.replace(" ", "-")
+    return out_str
+
+
+def main() -> None:
+    front_matter = (
+        "---\n"
+        "layout: post\n"
+        f"category: \"{modem_brand}\"\n"
+        f"title: \"{modem_model}\"\n"
+        f"date: \"{post_datetime}\"\n"
+        "---"
+    )
+    post_template = """
+<img src="{{ "assets/img/XXXXXX_front.jpg" | relative_url }}" class="modem_image">
+<img src="{{ "assets/img/XXXXXX_rear.jpg" | relative_url }}" class="modem_image">
+
+### Capabilities
+
+| Service | Supported |
+| :-: | :-: |
+| ADSL | XXX |
+| VDSL | XXX |
+| FIBRE | XXX |
+| VoIP | XXX |
+
+### Login Information
+
+| Provider | Username | Password | IP Address |
+| :-: | :-: | :-: | :-: |
+| Provider | Username | Password | IP_Address |
+
+### Supporting Resources
+
+| Resource | Link |
+| :-: | :-: |
+| Resource | [Link]() |
+"""
+    post_file_name = f"{post_date}-{safe_str(modem_brand)}-{safe_str(modem_model)}.md"
+    post_file_path = join(POST_DIR, post_file_name)
+    
+    with open(post_file_path, "w") as outfile:
+        outfile.writelines(front_matter + post_template)
+
+
+if __name__ == "__main__":
+    arg_size = len(argv) - 1
+    
+    if arg_size < 2:
+        err_str = (
+            f"2 arguments need be given, {arg_size} provided\n"
+            "Your command should read \"./newpost.py modem_brand modem_model\"\n"
+        )
+        raise ValueError(err_str)
+    
+    modem_brand = argv[1]
+    modem_model = argv[2]
+    post_datetime = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    post_date = post_datetime[:10]
+
+    main()
